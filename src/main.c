d410863d5fb6179eeb01a2fb3a6cf6e62abacba8
Andrew Thiele <308butterfly@gmail.com>
2021-01-17 00:19:15 -0600

    initial window code

--- src/main.c
+++ src/main.c
@@ -8,6 +8,18 @@
 
 int main(int argc, char** argv)
 {
+  SDL_Init(SDL_INIT_EVERYTHING);
+  SDL_Window* window = SDL_CreateWindow(
+    "Chip8 Window",
+    SDL_WINDOWPOS_UNDEFINED,
+    SDL_WINDOWPOS_UNDEFINED,
+    640,
+    320,
+    SDL_WINDOW_SHOWN
+  );
+
   printf("word..........\n");
+
+  SDL_DestroyWindow(window);
   return EXIT_SUCCESS;
 }
