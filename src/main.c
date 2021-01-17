17448e7d83a1601c8ddc71bb7589c5bfbdf6e104
Andrew Thiele <308butterfly@gmail.com>
2021-01-17 00:41:45 -0600

    handle SDL events

--- src/main.c
+++ src/main.c
@@ -17,9 +17,32 @@ int main(int argc, char** argv)
     320,
     SDL_WINDOW_SHOWN
   );
+  SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_TEXTUREACCESS_TARGET);
 
-  printf("word..........\n");
+  while (TRUE)
+  {
+    SDL_Event event;
+    while (SDL_PollEvent(&event))
+    {
+      if (event.type == SDL_QUIT)
+      {
+        goto out;
+      }
+    }
 
+    // renders color black
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    SDL_RenderClear(renderer); // paint over entire screen
+    SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0); // set to white
+    SDL_Rect r;
+    r.x = 0;
+    r.y = 0;
+    r.w = 40;
+    r.h = 40;
+    SDL_RenderFillRect(renderer, &r);
+    SDL_RenderPresent(renderer);
+  }
+out:
   SDL_DestroyWindow(window);
   return EXIT_SUCCESS;
 }
