fe841cd1254d2f6b8a9721ea53b67b9a4c9d4062
Andrew Thiele <308butterfly@gmail.com>
2021-01-17 00:10:45 -0600

    init commit

---
+++ src/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+#include "SDL2/SDL.h"
+
+#define TRUE 1
+#define FALSE 0
+#define EXIT_SUCCESS 0
+
+int main(int argc, char** argv)
+{
+  printf("word..........\n");
+  return EXIT_SUCCESS;
+}
